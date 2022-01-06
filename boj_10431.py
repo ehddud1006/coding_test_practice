3e260fef7fd4ba4e33536b84d7d099dbf37d93fe
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-07 02:35:15 +0900

    Create boj_10431.py

---
+++ boj_10431.py
@@ -0,0 +1,23 @@
+N = int(input())
+ans = []
+
+
+for i in range(N):
+    line = list(map(int,input().split()))
+    line.pop(0)
+    Slist = []
+    step = 0
+    # 제일앞 원소 제거
+    # print(line)
+    
+    for j in range(20) :
+        Slist.append(line[j])
+        Slist.sort()
+        for k in range(j+1) :
+            if(Slist[k]==line[j]) :
+                step += j-k
+                break
+    ans.append(step)
+
+for i in range(N):
+    print(f'{i+1} {ans[i]}')
