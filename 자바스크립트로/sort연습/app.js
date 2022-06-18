3dafedeff0b68b6513b549fc6e3438c793b62391
Kim Dong Young <ehddud1006@pusan.ac.kr>
2022-06-18 23:52:07 +0900

    ps

--- 자바스크립트로/sort연습/app.js
+++ 자바스크립트로/sort연습/app.js
@@ -10,7 +10,7 @@ employees.sort(function compare(a, b) {
 
 console.log(employees);
 /* result
- [
+ [d
   { name: '홍길동', salary: 30000 },
   { name: '마이콜', salary: 20000 },
   { name: '김길동', salary: 10000 },
