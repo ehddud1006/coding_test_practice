9e604cfd6c9e4d06ec48bbfcd36ce007340a3308
Kim Dong Young <ehddud1006@pusan.ac.kr>
2022-06-27 23:49:40 +0900

    ps

--- 자바스크립트로/app.js
+++ 자바스크립트로/app.js
@@ -1,18 +1,1 @@
-n = 10;
-function solution(n) {
-  var answer = 0;
-  let arr = new Array(n + 1).fill(true);
-
-  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
-    if (!arr[i]) continue;
-
-    for (let j = i * i; j <= n; j += i) {
-      arr[j] = false;
-    }
-  }
-  for (let i = 2; i <= n; i++) {
-    if (arr[i]) answer += 1;
-  }
-  return answer;
-}
-solution(n);
+·
