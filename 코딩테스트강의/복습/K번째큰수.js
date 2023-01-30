651d56a4e94a0ec7e1abf4e214b7e0e17f1b389d
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-01-30 23:58:30 +0900

    K번쨰큰수

--- 코딩테스트강의/복습/K번째큰수.js
+++ 코딩테스트강의/복습/K번째큰수.js
@@ -2,25 +2,30 @@ const N = 10;
 const K = 3;
 const arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
 
-const test = [1, 2, 3, 4, 5];
-const visit = Array.from({ length: test.length + 1 }, () => false);
+const visit = Array.from({ length: arr.length + 1 }, () => false);
 const temp = [];
-const answer = [];
+let answer = [];
 
-const dfs = L => {
+const dfs = (L, S) => {
   if (L === 3) {
-    console.log(temp);
+    const sum = temp.reduce((res, number) => (res += number), 0);
+    answer.push(sum);
     return;
   }
-  for (let i = 0; i < test.length; i++) {
+  for (let i = S; i < arr.length; i++) {
     if (!visit[i]) {
       visit[i] = true;
-      temp.push(test[i]);
-      dfs(L + 1);
+      temp.push(arr[i]);
+      dfs(L + 1, i + 1);
       visit[i] = false;
       temp.pop();
     }
   }
 };
 
-dfs(0);
+dfs(0, 0);
+
+answer = [...new Set(answer)];
+answer.sort((a, b) => b - a);
+
+console.log(answer[K - 1]);
